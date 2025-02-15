912f7183ee0a5eac827a9044e9cc333d06ff2451
emart126 <125086504+emart126@users.noreply.github.com>
2025-02-15 12:15:04 -0800

    added simple register and login test UI

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,74 @@
 import { Text, View, StyleSheet } from "react-native";
+import React, { useRef, useCallback, useMemo, useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Profile() {
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [registerMessage, setRegisterMessage] = useState('');
+
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const [loginMessage, setLoginMessage] = useState('');
+
+  const registerSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const registerResponse = await axios.post('http://127.0.0.1:5000/register', {username :username, email: email, password: password});
+      console.log(registerResponse);
+      setUsername('');
+      setEmail('');
+      setPassword('');
+      setRegisterMessage(registerResponse.data.message || 'Registration successful!');
+    } catch (error) {
+      console.error(error);
+      setRegisterMessage('Registration failed. Please try again.');
+    }
+  };
+
+  const submitLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const loginResponse = await axios.post('http://127.0.0.1:5000/login', {email: loginEmail, password: loginPassword});
+      console.log(loginResponse);
+      setLoginEmail('');
+      setLoginPassword('');
+      setLoginMessage(loginResponse.data.message || 'Login successful!');
+    } catch (error) {
+      console.error(error);
+      setLoginMessage('Login failed. Please try again.');
+    }
+  };
+
   return (
     <View
       style ={styles.container}
     >
+      <div>
+        Register: <br/>
+        Username: <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/><br/>
+        Email: <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/><br/>
+        Password: <input type="text" value={password} onChange={(e) => setPassword(e.target.value)}/><br/>
+        <button onClick={registerSubmit}>Register</button>
+        {registerMessage && <p>{registerMessage}</p>}
+      </div><br/>
+
+      <Text>
+        ====
+      </Text>
+
+      <div>
+        Login: <br/>
+        Email: <input type="text" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)}/><br/>
+        Password: <input type="text" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)}/><br/>
+        <button onClick={submitLogin}>Login</button>
+        {loginMessage && <p>{loginMessage}</p>}
+      </div>
+      
     </View>
   );
 }
